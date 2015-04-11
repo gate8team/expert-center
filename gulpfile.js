d6d3413cadb9b1c055e60403635279737c152923
ashabanov <me.antony.rain@gmail.com>
2015-04-11 15:16:51 +0300

    Added images, sprites and some gulp tasks.

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+  spritesmith = require('gulp.spritesmith'),
+  imagemin = require('gulp-imagemin'),
+  csso = require('gulp-csso'),
+  replace = require('gulp-replace'),
+  coffeelint = require('gulp-coffeelint'),
+  jshint = require('gulp-jshint'),
+  sass = require('gulp-sass');
+
+gulp.task('default',['sprite']);
+
+// Sprite task
+gulp.task('sprite', function () {
+  var spriteConfig = {
+      imgName: 'icons-sprite.png',
+      cssName: 'icons-sprite.css'
+    },
+    spriteData = gulp.src('src/static/img/icons/*').pipe(spritesmith(spriteConfig));
+
+  // Let's optimize images
+  spriteData.img
+    .pipe(imagemin())
+    .pipe(gulp.dest('src/static/img/'));
+
+  // Let's optimize css
+  spriteData.css
+    .pipe(csso())
+    .pipe(replace(/url\(\S+\)/g, 'url("../../img/icons-sprite.png")'))
+    .pipe(gulp.dest('src/static/styles/css/'));
+});
+
+// sass
+gulp.task('sass', function () {
+  gulp.src('src/static/styles/scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('src/static/styles/css/'));
+});
+
+// Let's lint our js
+gulp.task('js-lint', function() {
+  gulp.src('app/assets/javascripts/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter());
+});
